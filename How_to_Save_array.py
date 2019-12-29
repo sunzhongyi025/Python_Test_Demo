bb90f75b142153027ee407e6a221f96226237846
sunzhongyi025 <sunzhongyi025@aliyun.com>
2019-12-29 15:35:13 +0900

    Study numpy Basic function_2  sort, clip,

--- How_to_Save_array.py
+++ How_to_Save_array.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 
+print('------------------')
 # array = np.array([[1, 2, 3],
 #                  [4, 5, 6],
 #                  [5, 8, 9],
@@ -18,17 +19,30 @@ import numpy as np
 # a = np.arange(10, 26, 3).reshape((2, 3))
 # a = np.linspace(0, 11, 20).reshape((10, 2))
 # print(a)
-
-
-#a = np.array([[10, 20, 30, 40], [1, 2, 3, 4],
+print('------------------')
+# a = np.array([[10, 20, 30, 40], [1, 2, 3, 4],
 #              [5, 6, 7, 8], [11, 22, 33, 44]])
-#b = np.arange(16).reshape((4, 4))
+# b = np.arange(16).reshape((4, 4))
 
-#d = np.dot(a, b)  # 矩阵运算
-#d = a * b  # 普通运算
-#print(d < 20)
-#print(d)
+# d = np.dot(a, b)  # 矩阵运算
+# d = a * b  # 普通运算
+# print(d < 20)
+# print(d)
+print('------------------')
+# a = np.random.random((2, 4))
+# print(np.sum(a, axis=1))
+# print(np.min(a, axis=0))
 
-#a = np.random.random((2, 4))
-#print(np.sum(a, axis=1))
-#print(np.min(a, axis=0))
+print('------------------')
+#a = np.arange(2, 14).reshape((3, 4))
+
+#print(np.argmin(a))
+#print(np.mean(a, axis=0))
+#print(np.median(a))
+#print(np.cumsum(a))
+#print(np.diff(a, axis=0))
+#print(np.nonzero(a))
+
+#print(a.T)  # 行列变换
+#print(np.clip(a, 10, 11)) # 小于10的全是10，大于10的全是11
+
