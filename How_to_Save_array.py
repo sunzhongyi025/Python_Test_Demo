dd7b8435239357632a22ae6d2c8ab373a34df843
sunzhongyi025 <sunzhongyi025@aliyun.com>
2019-12-29 15:01:36 +0900

    Study numpy

---
+++ How_to_Save_array.py
@@ -0,0 +1,29 @@
+## numpy & pandas
+import pandas as pd
+import numpy as np
+
+# array = np.array([[1, 2, 3],
+#                  [4, 5, 6],
+#                  [5, 8, 9],
+#                  [2, 5, 9]],
+#                 dtype=np.float32)
+# print(array.dtype)
+# print('number of dem:', array.ndim)
+# print('shape of dem:', array.shape)
+# print('size:', array.size)
+
+# a = np.empty((5, 6), dtype=np.int)
+# print(a)
+
+# a = np.arange(10, 26, 3).reshape((2, 3))
+# a = np.linspace(0, 11, 20).reshape((10, 2))
+# print(a)
+
+
+a = np.array([10, 20, 30, 40])
+b = np.arange(4)
+c = a ** b
+d
+print(c)
+
+
