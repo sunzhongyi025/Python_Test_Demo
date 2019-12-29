94ccc6395f5c3563710a397fadc2e39fb04d18d6
sunzhongyi025 <sunzhongyi025@aliyun.com>
2019-12-29 12:11:11 +0900

    读取数据已经成功，现在在研究如何改变时间变量

--- NC_READ_Test.py
+++ NC_READ_Test.py
@@ -1,2 +1,15 @@
 import netCDF4 as nc
+
 File_test = nc.Dataset('D:\Studies\Python_Sun\PY_NE_Test/CLASS-CTEM_S3_npp.nc')
+print(File_test.variables)
+print(File_test.variables.keys())
+NPP = File_test.variables['npp']
+CLASS_CTM_NPP = NPP[:]
+print(CLASS_CTM_NPP.shape)
+
+Lon = File_test.variables['lon'][:]
+Lat = File_test.variables['lat'][:]
+time = File_test.variables['time']
+
+
+times = nc.num2date(time[:], time.units)
